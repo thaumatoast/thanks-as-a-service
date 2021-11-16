551228e4b93d781d68c299d080e0b37a6da48a59
Martin Wiseman <7862511+thaumatoast@users.noreply.github.com>
2021-11-16 16:34:49 -0500

    Update thanks.js

--- thanks.js
+++ thanks.js
@@ -3,10 +3,25 @@ const http = require('http');
 const hostname = 'thanksasaservice.com';
 const port = 80;
 
+let r = ''
+r += '<html>'
+r += '<head>'
+r += '<title>thanks</title>'
+r += '<meta name="description" content="thanks">'
+r += '<meta name="keywords" content="thanks">'
+r += '<meta property="og:title" content="thanks">'
+r += '<meta property="og:description" content="thanks">'
+r += '<meta property="og:type" content="website">'
+r += '<meta name="twitter:title" content="thanks">'
+r += '<meta name="twitter:description" content="thanks">'
+r += '</head>'
+r += '<body>thanks</body>'
+r += '</html>'
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<html><head><meta name="description" content="thanks"></head><body>thanks</body></html>');
+  res.end(r);
 });
 
 server.listen(port, hostname, () => {
