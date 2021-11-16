e28a688c3d6cfa877b591f6343e5fe8d23e2639a
Martin Wiseman <7862511+thaumatoast@users.noreply.github.com>
2021-11-16 16:04:14 -0500

    Update and rename server.js to thanks.js

--- thanks.js
+++ thanks.js
@@ -5,8 +5,8 @@ const port = 80;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('thanks');
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html><head><meta name="description" content="thanks"></head><body>thanks</body></html>');
 });
 
 server.listen(port, hostname, () => {
